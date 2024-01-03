359b03cd448b895a57ce66da021ab84b09b961a8
Moharabie99 <127060071+Moharabie99@users.noreply.github.com>
2024-01-03 09:10:32 +0200

    Update NashHousing.sql

--- Code/NashHousing.sql
+++ Code/NashHousing.sql
@@ -1,100 +1,26 @@
--- Data cleaning for Nashcville housing report
-
--- Standrize date formate for SaleDate
-
-ALTER TABLE nashhousing
-ADD ChangedSaleDate Date;
+-- Corrected spelling
+ALTER TABLE NashHousing
+ADD OwnerCity NVARCHAR(300);
 
 UPDATE NashHousing
-SET ChangedSaleDate = CONVERT(DATE, SaleDate)
-
---Populate PropertyAddress column
-
-UPDATE one
-SET PropertyAddress =  isnull (one.PropertyAddress,two.PropertyAddress)
-FROM NashHousing one
-INNER JOIN NashHousing two
-	
-	ON one.[UniqueID ] <> two.[UniqueID ]
-	AND
-	one.ParcelID = two.ParcelID
--- Spliting the PropertyAddress and OwnerAddress coulmns into (Street, City, State) columns
+SET OwnerCity = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2)
 
 ALTER TABLE NashHousing
-ADD PropertySplitAddress NVARCHAR(300);
+ADD OwnerState NVARCHAR(300);
 
 UPDATE NashHousing
-SET PropertySplitAddress = PARSENAME(REPLACE(PropertyAddress,',','.' ),2)
-
-ALTER TABLE NashHousing
-ADD PropertyCity NVARCHAR(300);
-
+SET OwnerState = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+-- Corrected update for OwnerState
 UPDATE NashHousing
-SET PropertyCity = PARSENAME(REPLACE(PropertyAddress,',','.' ),1)
-
--- Spliting the OwnerAddress column to (Address, City, State) columns
-
-ALTER TABLE NashHousing
-ADD OwnnerSplitAddress NVARCHAR(300);
-
-UPDATE NashHousing
-SET OwnnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3)
-
-ALTER TABLE NashHousing
-ADD OwnnerCity NVARCHAR(300);
-
-UPDATE NashHousing
-SET OwnnerCity = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2)
-
-ALTER TABLE NashHousing
-ADD OwnnerState NVARCHAR(300);
-
-UPDATE NashHousing
-SET OwnnerCity = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
-
--- Changing the 'N' and 'Y' in the SoldAsVacant column to 'Yes' and 'No'
-
-UPDATE NashHousing
-SET SoldAsVacant = CASE 
-	WHEN SoldAsVacant = 'y' THEN 'Yes'
-	WHEN SoldAsVacant = 'n' THEN 'No'
-	ELSE SoldAsVacant
-END
-
-SELECT DISTINCT SoldAsVacant, COUNT(SoldAsVacant) n
-FROM NashHousing
-GROUP BY SoldAsVacant
-ORDER BY n
-
--- Removing duplicate columns
-WITH NumRowCTE AS (
-SELECT *, 
-ROW_NUMBER() OVER (PARTITION BY ParcelID,
-								PropertyAddress,
-								Saleprice,
-								SaleDate,
-								LegalReference
-								ORDER BY
-								UniqueID) as row_num
-FROM NashHousing
-)
-
-select *
-FROM NumRowCTE
-WHERE row_num > 1
-ORDER BY PropertyAddress
-
--- Deleting unused columns
-
+SET OwnerState = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+-- Include PropertyAddress in the DROP COLUMN statement
 ALTER TABLE NashHousing
 DROP COLUMN SaleDate, PropertyAddress, OwnerAddress
-
--- Rename some columns for more efficency
-
+-- Example of renaming column
 EXEC sp_RENAME 'NashHousing.ChangedSaleDate', 'SaleDate', 'COLUMN'
-EXEC sp_RENAME 'NashHousing.PropertySplitAddress', 'PropertyAddress', 'COLUMN'
-EXEC sp_RENAME 'NashHousing.OwnnerSplitAddress', 'OwnerAddress', 'COLUMN'
-
-SELECT *
-FROM NashHousing
-
+-- Consider using ALTER TABLE...RENAME COLUMN
+ALTER TABLE NashHousing
+RENAME COLUMN ChangedSaleDate TO SaleDate;
+-- Trim spaces in SoldAsVacant
+UPDATE NashHousing
+SET SoldAsVacant = TRIM(SoldAsVacant)
